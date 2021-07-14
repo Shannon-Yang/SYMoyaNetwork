70660081be9cda40f9c74b7c46c19518694cab5b
Shannon Yang <shannonyang0805@gmail.com>
2021-07-14 14:59:34 +0800

    add file

---
+++ SYMoyaNetwork/NetworkConfig.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkConfig.swift
+//  SYMoyaNetwork
+//
+//  Created by ShannonYang on 2021/7/14.
+//  Copyright © 2021 Shannon Yang. All rights reserved.
+//
+
+import Foundation
---
+++ SYMoyaNetwork/SYMoyaTargetType.swift
@@ -0,0 +1,9 @@
+//
+//  SYMoyaTargetType.swift
+//  SYMoyaNetwork
+//
+//  Created by ShannonYang on 2021/7/14.
+//  Copyright © 2021 Shannon Yang. All rights reserved.
+//
+
+import Foundation
