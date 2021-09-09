7cce1bef0e9a761c53f096bdfd8093a2bf41fd65
ShannonYang <shannon@ShannonYangdeMacBook-Pro.local>
2021-09-09 18:52:52 +0800

    add batch request agent

---
+++ SYMoyaNetwork/BatchRequest/SYMoyaBatchRequestProviderAgent.swift
@@ -0,0 +1,23 @@
+//
+//  SYMoyaBatchRequestProviderAgent.swift
+//  SYMoyaNetwork
+//
+//  Created by ShannonYang on 2021/9/9.
+//  Copyright © 2021 Shannon Yang. All rights reserved.
+//
+
+import Foundation
+
+struct SYMoyaBatchRequestProviderAgent {
+    
+    static let sharedInstance : SYMoyaBatchRequestProviderAgent = SYMoyaBatchRequestProviderAgent()
+    
+    func response(providers: [SYMoyaProvider<Target>]) where Target: SYTargetType {
+        
+    
+    }
+    
+    
+    
+    
+}
---
+++ SYMoyaNetwork/BatchRequest/SYMoyaProvider+BatchRequest.swift
@@ -0,0 +1,26 @@
+//
+//  SYMoyaProvider+BatchRequest.swift
+//  SYMoyaNetwork
+//
+//  Created by ShannonYang on 2021/9/9.
+//  Copyright © 2021 Shannon Yang. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+extension SYMoyaProvider {
+    
+    
+    
+    
+    func addBatchProvider(provider: SYMoyaProvider) {
+        
+    }
+    
+    func removeBatchProvider(provider: SYMoyaProvider) {
+        
+    }
+    
+    
+}
