c2ea6cb197ced216bbc5d48e21c4d009bd9e603f
Shannon Yang <shannonyang0805@gmail.com>
2022-07-11 23:00:15 +0800

    Added MJExtension support

---
+++ Sources/MJExtension/Response+MJExtension.swift.swift
@@ -0,0 +1,32 @@
+//
+//  Response+MJExtension.swift.swift
+//  ZaoZi
+//
+//  Created by Shannon Yang on 2022/1/4.
+//  Copyright © 2022 Shenzhen JUJUBE Health Technology Co.,Ltd. All rights reserved.
+//
+
+import Foundation
+import Moya
+import MJExtension
+
+//MARK: - MJExtension
+public extension Response {
+    
+    /// Convert response to MJExtension format data
+    /// - Parameters:
+    ///   - type: Subclass object that inherits NSObject
+    /// - Returns: The converted MJExtension data
+    func mapObject<T: NSObject>(_ type: T.Type) -> T? {
+        return type.mj_object(withKeyValues: self.data)
+    }
+
+    /// Convert response to MJExtension format data array
+    /// - Parameters:
+    ///   - type: Subclass object that inherits NSObject
+    /// - Returns: The converted MJExtension data array
+    func mapArray<T: NSObject>(_ type: T.Type)  -> [T]? {
+        return type.mj_objectArray(withKeyValuesArray: self.data) as? [T]
+    }
+    
+}
