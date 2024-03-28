49bb9928fdd7b2ebc3e85efdb3be255a422eef97
Shannon Yang <shannonyang0805@gmail.com>
2024-03-28 18:45:22 +0800

    doc shell

--- doc.sh
+++ doc.sh
@@ -1,7 +1,18 @@
+#swift build
+targets=("SYMoyaNetwork" "SYMoyaReactiveHandyJSON" "SYMoyaReactiveObjectMapper" "SYMoyaHandyJSON" "SYMoyaRxHandyJSON" "SYMoyaRxObjectMapper" "SYMoyaObjectMapper" "ReactiveSYMoyaNetwork" "RxSYMoyaNetwork") 
+for arg in ${targets[*]}; do 
+ if [ ! -d "/docs/$arg/" ]; then
+  pwd
+  cd docs/
+  sudo mkdir /$arg
+  
+  chmod 777 /$arg
+fi
+     swift package --allow-writing-to-directory ./docs \
+      generate-documentation --target $arg \
+       --disable-indexing \
+       --transform-for-static-hosting \
+       --hosting-base-path SYMoyaNetwork \
+       --output-path ./$arg/docs  
+done
 
-swift package --allow-writing-to-directory ./docs \
-    generate-documentation --target SYMoyaNetwork \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path SYMoyaNetwork \
-    --output-path ./docs
