15237c2364854c8e4ceb957fe9b033c7e8433236
Shannon Yang <shannonyang0805@gmail.com>
2024-03-28 15:57:00 +0800

    Update GitHub pages documentation site.

--- doc.sh
+++ doc.sh
@@ -1,7 +1,7 @@
 
-swift package --allow-writing-to-directory /docs \
+swift package --allow-writing-to-directory ./docs \
     generate-documentation --target SYMoyaNetwork \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path https://shannon-yang.github.io/SYMoyaNetwork/ \
-    --output-path /docs
+    --output-path ./docs
