9519489c7650f90b59b34a5cc9261a531c71c783
Shannon Yang <shannonyang0805@gmail.com>
2024-03-29 10:23:47 +0800

    doc c update

--- doc.sh
+++ doc.sh
@@ -1,18 +1,16 @@
-#swift build
+swift build
 targets=("SYMoyaNetwork" "SYMoyaReactiveHandyJSON" "SYMoyaReactiveObjectMapper" "SYMoyaHandyJSON" "SYMoyaRxHandyJSON" "SYMoyaRxObjectMapper" "SYMoyaObjectMapper" "ReactiveSYMoyaNetwork" "RxSYMoyaNetwork") 
 for arg in ${targets[*]}; do 
  if [ ! -d "/docs/$arg/" ]; then
-  pwd
   cd docs/
-  sudo mkdir /$arg
-  
-  chmod 777 /$arg
+  sudo mkdir $arg
+  cd ..
 fi
      swift package --allow-writing-to-directory ./docs \
       generate-documentation --target $arg \
        --disable-indexing \
        --transform-for-static-hosting \
        --hosting-base-path SYMoyaNetwork \
-       --output-path ./$arg/docs  
+       --output-path ./docs/$arg 
 done
 
