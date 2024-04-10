506f1d32b90c9608114a6cf84d6e0f0f928eb649
Shannon Yang <shannonyang0805@gmail.com>
2024-04-10 09:48:05 +0800

    fix pod error

--- Sources/SYMoyaNetwork/Response/SYDataResponse.swift
+++ Sources/SYMoyaNetwork/Response/SYDataResponse.swift
@@ -64,24 +64,6 @@ extension SYDataResponse {
         SYDataResponse<NewSuccess, Failure>(resultResponse: resultResponse, result: result.map(transform))
     }
 
-    /// Evaluates the given closure when the result of this `SYDataResponse` is a success, passing the unwrapped result
-    /// value as a parameter.
-    ///
-    /// Use the `tryMap` method with a closure that may throw an error. For example:
-    ///
-    ///     let possibleData: SYDataResponse<JSON> = ...
-    ///     let possibleObject = possibleData.tryMap {
-    ///         try JSONSerialization.jsonObject(with: $0)
-    ///     }
-    ///
-    /// - parameter transform: A closure that takes the success value of the instance's result.
-    ///
-    /// - returns: A success or failure `SYDataResponse` depending on the result of the given closure. If this instance's
-    ///            result is a failure, returns the same failure.
-    public func tryMap<NewSuccess>(_ transform: (Success) throws -> NewSuccess) -> SYDataResponse<NewSuccess, Error> {
-        SYDataResponse<NewSuccess, Error>(resultResponse: resultResponse, result: result.tryMap(transform))
-    }
-
     /// Evaluates the specified closure when the `SYDataResponse` is a failure, passing the unwrapped error as a parameter.
     ///
     /// Use the `mapError` function with a closure that does not throw. For example:
@@ -94,21 +76,5 @@ extension SYDataResponse {
     /// - Returns: A `SYDataResponse` instance containing the result of the transform.
     public func mapError(_ transform: (Error) -> Error) -> SYDataResponse<Success, Error> {
         SYDataResponse<Success, Error>(resultResponse: resultResponse, result: result.mapError(transform))
-    }
-
-    /// Evaluates the specified closure when the `SYDataResponse` is a failure, passing the unwrapped error as a parameter.
-    ///
-    /// Use the `tryMapError` function with a closure that may throw an error. For example:
-    ///
-    ///     let possibleData: SYDataResponse<Data> = ...
-    ///     let possibleObject = possibleData.tryMapError {
-    ///         try someFailableFunction(taking: $0)
-    ///     }
-    ///
-    /// - Parameter transform: A throwing closure that takes the error of the instance.
-    ///
-    /// - Returns: A `SYDataResponse` instance containing the result of the transform.
-    public func tryMapError(_ transform: (Error) throws -> Error) -> SYDataResponse<Success, Error> {
-        SYDataResponse<Success, Error>(resultResponse: resultResponse, result: result.tryMapError(transform))
     }
 }
